831d39af9fea5be14c70958155b5888c46281fb5
yevdokymenko90 <yevdokymenko90@gmail.com>
2024-03-09 20:21:10 +0100

    docker python

---
+++ python-app/main.py
@@ -0,0 +1,11 @@
+import calendar
+
+print('SuperCalendar!')
+print(calendar.month(2021, 1))
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+print(calendar.month(year, month))
+
+print('Goodbye!')
